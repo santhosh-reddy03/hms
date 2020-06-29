3f34e4885aebfaae1ab3d5ef7395d0553c56cf97
7scythe7 <jintojosan@gmail.com>
2020-06-29 11:30:51 +0530

    crud operations for patients

--- application/forms.py
+++ application/forms.py
@@ -1,9 +1,10 @@
 
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, IntegerField, SelectField, TextAreaField
-from wtforms.fields.html5 import DateField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField,  SelectField, TextAreaField,DateField
 from wtforms.validators import InputRequired, Length, NumberRange, Optional, Regexp
+
+from datetime import datetime
 
 
 class LoginForm(FlaskForm):
@@ -13,7 +14,7 @@ class LoginForm(FlaskForm):
 
 
 class RegisterationForm(FlaskForm):
-    patient_ssn = StringField("Patient SSN Id*", validators=[InputRequired()], render_kw={'Placeholder': 'Patient Id'})
+    patient_ssn = StringField("Patient SSN Id*", validators=[InputRequired()], render_kw={'Placeholder': 'Patient SSN Id'})
     patient_name = StringField("Patient Name*", validators=[InputRequired(), Regexp(regex='^[A-Za-z]+$',message='Name should contain alphabets only')],render_kw={'Placeholder': 'Patient Name'})
     age = IntegerField('Age*', validators=[InputRequired()],render_kw={'Placeholder': 'Patient Id'})
     doa = DateField('Date of Admission*' , validators=[InputRequired()], format='%Y-%m-%d')
@@ -25,10 +26,10 @@ class RegisterationForm(FlaskForm):
     submit = SubmitField("Register")
 
 class UpdateForm(FlaskForm):
-    patient_ssn = StringField("Patient SSN Id*", validators=[InputRequired()],render_kw={'Placeholder': 'Patient Id'})
+    patient_ssn = StringField("Patient SSN Id*", validators=[InputRequired()],render_kw={'Placeholder': 'Patient SSN Id'})
     patient_name = StringField("Patient Name", validators=[Optional(),Regexp(regex='^[A-Za-z]+$',message='Name should contain alphabets only')],render_kw={'Placeholder': 'Patient Name'})
-    age = IntegerField("Age", validators=[Optional(),Length(min=3, max=3)],render_kw={'Placeholder': 'Age'})
-    doa = DateField("Date of Admission" ,validators=[Optional()] ,format='%Y-%m-%d')
+    age = StringField("Age", validators=[Optional(),Regexp(regex='^\d{1,3}$')],render_kw={'Placeholder': 'Age'})
+    doa = DateField("Date of Admission" ,validators=[Optional()] ,format='%Y-%m-%d',default=datetime.now())
     bed_type = SelectField("Bed Type",  validators=[Optional()],choices=[('GW', 'General Ward'), ('SS', 'Semi Sharing'), ('SR', 'Single Room')],
                                render_kw={'Placeholder': 'Select'})
     address = TextAreaField("Address" ,validators=[Optional()], render_kw={'Placeholder': 'Enter Address'})
@@ -36,3 +37,27 @@ class UpdateForm(FlaskForm):
     city = StringField("City", validators=[Optional()],render_kw={'Placeholder': 'City'})
     get = SubmitField("Get")
     update = SubmitField("Update")
+
+class DeleteForm(FlaskForm):
+    patient_ssn = StringField("Patient SSN Id*", validators=[InputRequired()],render_kw={'Placeholder': 'Patient SSN Id'})
+    patient_name = StringField("Patient Name", validators=[Optional(),Regexp(regex='^[A-Za-z]+$',message='Name should contain alphabets only')],render_kw={'readonly':True})
+    age = StringField("Age", validators=[Optional(),Regexp(regex='^\d{1,3}$')],render_kw={'readonly':True})
+    doa = DateField("Date of Admission" ,validators=[Optional()] ,format='%Y-%m-%d',default=datetime.now(),render_kw={'readonly':True})
+    bed_type = SelectField("Bed Type",  validators=[Optional()],choices=[('GW', 'General Ward'), ('SS', 'Semi Sharing'), ('SR', 'Single Room')],
+                               render_kw={'readonly':True})
+    address = TextAreaField("Address" ,validators=[Optional()], render_kw={'readonly':True})
+    state = StringField("State", validators=[Optional()],render_kw={'readonly':True})
+    city = StringField("City", validators=[Optional()],render_kw={'readonly':True})
+    get = SubmitField("Get")
+    delete = SubmitField("Delete")
+class SearchForm(FlaskForm):
+    patient_ssn = StringField("Patient SSN Id*", validators=[InputRequired()],render_kw={'Placeholder': 'Patient SSN Id'})
+    patient_name = StringField("Patient Name", validators=[Optional(),Regexp(regex='^[A-Za-z]+$',message='Name should contain alphabets only')],render_kw={'readonly':True})
+    age = StringField("Age", validators=[Optional(),Regexp(regex='^\d{1,3}$')],render_kw={'readonly':True})
+    doa = DateField("Date of Admission" ,validators=[Optional()] ,format='%Y-%m-%d',default=datetime.now(),render_kw={'readonly':True})
+    bed_type = SelectField("Bed Type",  validators=[Optional()],choices=[('GW', 'General Ward'), ('SS', 'Semi Sharing'), ('SR', 'Single Room')],
+                               render_kw={'readonly':True})
+    address = TextAreaField("Address" ,validators=[Optional()], render_kw={'readonly':True})
+    state = StringField("State", validators=[Optional()],render_kw={'readonly':True})
+    city = StringField("City", validators=[Optional()],render_kw={'readonly':True})
+    search = SubmitField("Search")
--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,10 @@
 from application import app
 from flask import render_template, flash, redirect, request, url_for, jsonify, session
 from sqlalchemy import text
-from application.forms import LoginForm, RegisterationForm,UpdateForm
+from application.forms import LoginForm, RegisterationForm,UpdateForm,DeleteForm,SearchForm
 from application import db
 from application.models import Patient
-import datetime
+from datetime import datetime
 from application.models import Userstore
 
 '''db.drop_all()
@@ -38,7 +38,7 @@ def login():
             session['user_id'] = form.login.data
             session['user_type'] = user_type[0]
             if user_type[0] == 'E':
-                return redirect(url_for('create_customer'))
+                return redirect(url_for('create_patient'))
             elif user_type[0] == 'P':
                 return "<h1>pharmacist</h1>"
             elif user_type[0] == 'D':
@@ -81,6 +81,21 @@ def create_patient():
         flash('You are not logged in ','danger')
         return redirect(url_for('login'))
 
+def set_details(form,ssn_flag):
+    sql = text("Select * From patients WHERE patient_ssn = :x ")
+    rslt = db.engine.execute(sql, x=form.patient_ssn.data)
+    details = [row for row in rslt]
+    #redirect(url_for('update_patient'))
+    form.patient_ssn.render_kw={"readonly":ssn_flag}
+    form.patient_name.data=details[0][2]
+    form.age.data=details[0][3]
+    obj = datetime.strptime(details[0][4], '%Y-%m-%d').date()
+    form.doa.data = obj #not able to change date
+    form.bed_type.data=details[0][5]
+    form.address.data=details[0][6]
+    form.state.data=details[0][7]
+    form.city.data=details[0][8]
+
 
 
 @app.route('/update_patient',methods=['GET', 'POST'])
@@ -96,48 +111,71 @@ def update_patient():
                 flash('Patient not found !','warning')
             else:
                 if form.get.data:
-                    sql = text("Select * From patients WHERE patient_ssn = :x ")
-                    rslt = db.engine.execute(sql, x=form.patient_ssn.data)
-                    details = [row for row in rslt]
-                    redirect(url_for('update_patient'))
-                    form.patient_ssn.data=details[0][0]
-                    form.patient_name.data=details[0][2]
-                    form.age.data=details[0][3]
-                    form.doa.data=details[0][4]
-                    form.bed_type.data=details[0][5]
-                    form.address.data=details[0][6]
-                    form.state.data=details[0][7]
-                    form.city.data=details[0][8]
+                    set_details(form,True)
+
                 elif form.update.data:
                     sql = text('UPDATE patients SET patient_name = :n, age = :ag, admission_date = :doa, bed_type = :bt, address = :ad, state = :s, city = :c  WHERE patient_ssn = :ssn')
-                    rslt = db.engine.execute(sql, n=form.patient_name.data, ag=form.age.data, doa=form.doa.data, bt=form.bed_type.data, ad=form.address.data, s=form.state.data, c=form.city.data)
+                    rslt = db.engine.execute(sql, n=form.patient_name.data, ag=int(form.age.data), doa=form.doa.data, bt=form.bed_type.data, ad=form.address.data, s=form.state.data, c=form.city.data,ssn=form.patient_ssn.data)
+                    flash('Patient update initiated successfully','success')
+                    redirect(url_for('update_patient'))
 
         else:
             for key in form.errors:
                 flash('Invalid ' + key, 'danger')
-        return render_template('update_patient.html', form=form,title='Patient Registeration')
+        return render_template('update_patient.html', form=form,title='Update Patient Details')
     else:
         flash('You are not logged in ', 'danger')
         return redirect(url_for('login'))
 
 
-@app.route('/delete_patient')
+@app.route('/delete_patient', methods=['GET', 'POST'])
 def delete_patient():
     if 'user_id' in session and session['user_type'] == 'E':
         #code here
-        pass
+        form = DeleteForm()
+        if form.validate_on_submit():
+            sql = text("SELECT  patient_ssn FROM patients WHERE patient_ssn = :x ")
+            rslt = db.engine.execute(sql, x=form.patient_ssn.data)
+            name = [row[0] for row in rslt]
+            if len(name) == 0:
+                flash('Patient not found !','warning')
+            else:
+                if form.get.data:
+                    set_details(form,True)
 
+                elif form.delete.data:
+                    sql = text('UPDATE patients SET status = INACTIVE WHERE patient_ssn = :ssn')
+                    rslt = db.engine.execute(sql,ssn=form.patient_ssn.data)
+                    flash('“Patient deletion initiated successfully','success')
+                    redirect(url_for('delete_patient'))
+
+
+        else:
+            for key in form.errors:
+                flash('Invalid ' + key, 'danger')
+        return render_template('delete_patient.html', form=form,title='Delete Patient Details')
     else:
         flash('You are not logged in ', 'danger')
         return redirect(url_for('login'))
 
 
-@app.route('/search_patient')
+@app.route('/search_patient', methods=['GET', 'POST'])
 def search_patient():
     if 'user_id' in session and session['user_type'] == 'E':
         #code here
-        pass
-
+        form = SearchForm()
+        if form.validate_on_submit():
+            sql = text("SELECT  patient_ssn FROM patients WHERE patient_ssn = :x ")
+            rslt = db.engine.execute(sql, x=form.patient_ssn.data)
+            name = [row[0] for row in rslt]
+            if len(name) == 0:
+                flash('Patient not found !','warning')
+            else:
+                set_details(form, False)
+        else:
+            for key in form.errors:
+                flash('Invalid ' + key, 'danger')
+        return render_template('search_patient.html', form=form,title='Search Patient Details')
     else:
         flash('You are not logged in ', 'danger')
         return redirect(url_for('login'))
